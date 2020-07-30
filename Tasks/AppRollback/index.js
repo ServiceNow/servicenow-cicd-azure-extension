0f7c3d1e9d676299e14d1ba884f9e0ca23e96209
Anton Malyi <amalyi@softserveinc.com>
2020-07-30 14:29:21 +0300

    Move index.js and test.js to be a symlink

--- Tasks/AppRollback/index.js
+++ Tasks/AppRollback/index.js
@@ -1,9 +1,1 @@
-
-try {
-    const task = require('./task'), pipeline = require('./AzureDevopsPipeline');
-    task.init(pipeline);
-    task.run().catch(()=>process.exit(1));
-} catch (e) {
-    console.error('The error is:', e);
-    process.exit(1);
-}
+C:/Projects/servicenow/cicdplugin/lib/index.js
--- Tasks/AppRollback/test.js
+++ Tasks/AppRollback/test.js
@@ -1,9 +1,1 @@
-try {
-    console.log('\x1b[34mTest mode!\x1b[0m\n')
-    const task = require('./task'), pipeline = require('../../lib/testPipeline');
-    task.init(pipeline);
-    task.run().catch(()=>process.exit(1));
-} catch (e) {
-    console.error('The error is:', e);
-    process.exit(1);
-}
+C:/Projects/servicenow/cicdplugin/lib/test.js
